6fa03713408bac1d75be9f5e213711250da4b506
Ajay Singh <ajayssinghh@gmail.com>
2021-09-01 13:10:05 +0530

    Create column.js

---
+++ src/Table/column.js
@@ -0,0 +1,19 @@
+export const columns = [{  
+        Header: 'Name',  
+        accessor: 'name'  
+    },{  
+        Header: 'Code',  
+        accessor: 'code'  
+    },{  
+        Header: 'Availability',  
+        accessor: 'availability'  
+    },{  
+        Header: 'Need to Repair',  
+        accessor: "needing_repair"  
+    },{  
+        Header: 'Durability',  
+        accessor: "durability"  
+    },{  
+        Header: 'Mileage',  
+        accessor: "mileage"  
+    }]  
