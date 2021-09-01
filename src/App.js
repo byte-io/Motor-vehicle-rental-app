40f621f672ea4d7c9277d0df8ee6d01afe219c1a
Ajay Singh <ajayssinghh@gmail.com>
2021-09-01 12:44:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React from "react";
+import Table from './Table/Table';
+import Footer from './Component/Footer';
+import Header from './Component/Header';
+ 
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <Header/> 
+        <Table/>
+        <Footer/>
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
 
